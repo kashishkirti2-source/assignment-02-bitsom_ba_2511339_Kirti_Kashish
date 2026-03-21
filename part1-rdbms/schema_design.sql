575524489c31fe296e2b7c533da3646b694004f3
Kashish Kirti <kashishkirti2@gmail.com>
2026-03-21 13:56:03 +0530

    Update schema_design.sql

--- part1-rdbms/schema_design.sql
+++ part1-rdbms/schema_design.sql
@@ -1,1 +1,43 @@
+-- Customers Table
+CREATE TABLE Customers (
+    customer_id VARCHAR(10) PRIMARY KEY,
+    customer_name VARCHAR(100) NOT NULL,
+    customer_email VARCHAR(100) NOT NULL,
+    customer_city VARCHAR(50) NOT NULL
+);
 
+-- Products Table
+CREATE TABLE Products (
+    product_id VARCHAR(10) PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL
+);
+
+-- Sales Representatives Table
+CREATE TABLE Sales_Reps (
+    sales_rep_id VARCHAR(10) PRIMARY KEY,
+    sales_rep_name VARCHAR(100) NOT NULL,
+    sales_rep_email VARCHAR(100) NOT NULL,
+    office_address VARCHAR(200) NOT NULL
+);
+
+-- Orders Table
+CREATE TABLE Orders (
+    order_id VARCHAR(10) PRIMARY KEY,
+    customer_id VARCHAR(10),
+    sales_rep_id VARCHAR(10),
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (sales_rep_id) REFERENCES Sales_Reps(sales_rep_id)
+);
+
+-- Order Items Table
+CREATE TABLE Order_Items (
+    order_id VARCHAR(10),
+    product_id VARCHAR(10),
+    quantity INT,
+    PRIMARY KEY (order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
